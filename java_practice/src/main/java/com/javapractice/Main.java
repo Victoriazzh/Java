63969f6352deb2805c9482c030d2c931f7b3d354
Victoria Zhang <zhangzhiheng1999@gmail.com>
2023-10-18 13:32:21 +0100

    first task added

--- java_practice/src/main/java/com/javapractice/Main.java
+++ java_practice/src/main/java/com/javapractice/Main.java
@@ -1,19 +1,29 @@
 package com.javapractice;
+import java.util.Scanner;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double cost;
+        double vat;
+        Scanner sc= new Scanner(System.in) ;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.printf("Enter a cost:");
+        cost=sc.nextDouble();
+        System.out.println(cost);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.printf("Enter VAT:");
+        vat=sc.nextDouble();
+        System.out.println(vat);
+
+        double finalVAT=VAT(vat,cost);
+        double finalPrice = finalVAT+cost;
+
+        System.out.printf("Total price:");
+        System.out.println(finalPrice);
+    }
+    static double VAT(double vat, double cost){
+       double final_vat = cost*(vat/100);
+
+       return final_vat;
     }
 }
