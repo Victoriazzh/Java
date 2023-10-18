c35bd4b2d9131b71d91b2484f0680bd37a7444a9
shivjulka <shivjulka@gmail.com>
2023-10-18 15:13:43 +0100

    add while loop

--- java_practice/src/main/java/com/javapractice/Main.java
+++ java_practice/src/main/java/com/javapractice/Main.java
@@ -1,4 +1,5 @@
 package com.javapractice;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
@@ -7,23 +8,49 @@ public class Main {
         double vat;
         Scanner sc= new Scanner(System.in) ;
 
-        System.out.printf("Enter a cost:");
-        cost=sc.nextDouble();
-        System.out.println(cost);
+        try {
+            System.out.printf("Enter a cost:");
+            cost=sc.nextDouble();
+            System.out.println(cost);
 
-        System.out.printf("Enter VAT:");
-        vat=sc.nextDouble();
-        System.out.println(vat);
+            System.out.printf("Enter VAT:");
+            vat=sc.nextDouble();
+            System.out.println(vat);
 
-        double finalVAT=VAT(vat,cost);
-        double finalPrice = finalVAT+cost;
+            double finalVAT=VAT(vat,cost);
+            double finalPrice = finalVAT+cost;
 
-        System.out.printf("Total price:");
-        System.out.println(finalPrice);
+            System.out.printf("Total price:");
+            System.out.println(finalPrice);
+
+            while (true) {
+                System.out.printf("Enter a cost:");
+                cost=sc.nextDouble();
+                System.out.println(cost);
+
+                System.out.printf("Enter VAT:");
+                vat=sc.nextDouble();
+                System.out.println(vat);
+
+                finalVAT=VAT(vat,cost);
+                finalPrice = finalVAT+cost;
+
+                System.out.printf("Total price:");
+                System.out.println(finalPrice);
+            }
+
+        }
+        catch(NoSuchElementException e) {
+            System.out.println("incorrect value");
+        }
+
+
     }
     static double VAT(double vat, double cost){
        double final_vat = cost*(vat/100);
 
        return final_vat;
     }
+
+
 }
