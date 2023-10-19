d6352c988e08434aac74ea23fed126a88e9bc580
shivjulka <shivjulka@gmail.com>
2023-10-19 15:52:20 +0100

    encapsulation test

--- java_practice/src/main/java/com/javapractice/Main.java
+++ java_practice/src/main/java/com/javapractice/Main.java
@@ -2,103 +2,120 @@ package com.javapractice;
 import java.text.CollationElementIterator;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        //declare variables
-        Scanner sc = new Scanner(System.in);
-        Scanner scString= new Scanner(System.in);
-        boolean continueCalculations = true;
-        double totalCost=0;
+//task 5
+class PurchasedItem {
+    private double costPrice,vatRate,totalCost,finalPrice;
+    private int quantity;
+    private ArrayList<Double> prices;
 
-        // ---------Only for task 3---------//
-        //static array that grows
-        //double[] prices = new double[5];
-        //int entries=0;
+    //initializes the dynamic array
+    public PurchasedItem(){
+        prices= new ArrayList<>();
+    }
 
-        //task 4 defining dynamic arrays
-        ArrayList<Double> prices = new ArrayList<>();
+    public void setItems(double costPrice, double vatRate, double totalCost, int quantity){
+        this.quantity = quantity;
+        this.vatRate=vatRate;
+        this.costPrice=costPrice;
+        this.totalCost=totalCost;
+    }
+    protected void getFinalPrice(){
+        double vatValue = costPrice*((vatRate/100)+1);
+        System.out.printf("Price per item %.2f %n",costPrice+vatValue);
+        finalPrice=(costPrice+vatValue)*quantity;
+        System.out.printf("This price for all the items are: £%.2f%n", finalPrice);
+        prices.add(finalPrice);
+    }
 
-        while (continueCalculations) {
-            double cost;
-            double vat;
-            int quantity;
-            String input;
+    protected double getTotalCost(){
+        totalCost+=finalPrice;
+        System.out.printf("the total cost is: £%.2f%n",totalCost);
+        return totalCost;
+    }
 
-            try {
-                System.out.print("Enter a cost: ");
-                cost = sc.nextDouble();
-                System.out.print("Enter VAT rate (%): ");
-                vat = sc.nextDouble();
-
-                System.out.println("Enter the quantity: ");
-                quantity=sc.nextInt();
-
-
-                // ---------Only for task 3---------//
-                //if the index is now the size of the array we must increment it
-                //if (entries== prices.length){
-                    //make prices the original prices with 2 more slots
-                   // prices= Arrays.copyOf(prices,prices.length+2);
-                //}
-
-                PurchasedItem item = new PurchasedItem(cost,vat,quantity);
-
-                double finalPrice = item.getTotalPrice();
-
-                totalCost+=finalPrice;
-
-                // add the final price to the prices array at the end
-                prices.add(finalPrice);
-
-
-
-                System.out.printf("This price for all the items are: £%.2f%n", finalPrice);
-                System.out.printf("the total cost is: £%.2f%n",totalCost);
-
-                System.out.print("Type QUIT to stop or press ENTER to continue: ");
-                input = scString.nextLine();
-
-                if (input.equalsIgnoreCase("QUIT")) {
-                    continueCalculations = false;
-                }
-
-                // ---------Only for task 3---------//
-                //increment entries
-                //entries++;
-
-            } catch (InputMismatchException e) {
-                System.out.println("Incorrect value. Please enter valid numbers.");
-                sc.nextLine(); // Consume the invalid input
-            }
-        }
-
-
-        //after quit print sorted array
-
-        //use collections to sort the array prices and then output it
-        Collections.sort(prices);
+    public void printList(){
         System.out.println("These are the sorted prices: ");
         for (double price : prices) {
             System.out.printf("£%.2f %n", price);
         }
     }
+
+    public void sortList(){
+        Collections.sort(prices);
+    }
+
 }
 
-//task 5
-class PurchasedItem {
-    private double costPrice;
-    private double vatRate;
+class userPrompt {
+    //declare variables
+    private Scanner sc = new Scanner(System.in);
+    private Scanner scString= new Scanner(System.in);
+    private double cost, vat;
     private int quantity;
+    private String input;
 
-    public PurchasedItem(double costPrice, double vatRate, int quantity){
-        this.quantity = quantity;
-        this.vatRate=vatRate;
-        this.costPrice=costPrice;
-    }
-    protected double getTotalPrice(){
-        double vatValue = costPrice*(vatRate/100);
-        System.out.printf("Price per item %.2f %n",costPrice+vatValue);
-        return (costPrice+vatValue)*quantity;
+    public double getCost(){
+        System.out.print("Enter a cost: ");
+        this.cost = sc.nextDouble();
+        return cost;
     }
 
+    public double getVat(){
+        System.out.print("Enter VAT rate (%): ");
+        this.vat = sc.nextDouble();
+        return vat;
+    }
+
+    public int getQuantity(){
+        System.out.print("Enter the quantity: ");
+        this.quantity=sc.nextInt();
+        return quantity;
+    }
+
+    public String getQuitStatus(){
+        System.out.print("Type QUIT to stop or press ENTER to continue: ");
+        input = scString.nextLine();
+        return input;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        //declare variables
+        boolean continueCalculations = true;
+        double totalCost=0;
+        double cost,vat;
+        int quantity;
+        String input;
+
+        userPrompt inputs = new userPrompt();
+        PurchasedItem item = new PurchasedItem();
+
+        while (continueCalculations) {
+            try {
+                cost=inputs.getCost();
+                vat= inputs.getVat();
+                quantity=inputs.getQuantity();
+
+                item.setItems(cost, vat, totalCost, quantity);
+                item.getFinalPrice();
+
+
+                totalCost=item.getTotalCost();
+                input=inputs.getQuitStatus();
+
+                if (input.equalsIgnoreCase("QUIT")) {
+                    continueCalculations = false;
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect value. Please enter valid numbers.");
+            }
+        }
+        //after quit print sorted array
+        //use collections to sort the array prices and then output it
+        item.sortList();
+        item.printList();
+    }
+    
 }
