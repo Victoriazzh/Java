e5a01cc7cfba6adcb4a420437f89568d361ce30a
shivjulka <shivjulka@gmail.com>
2023-10-19 11:33:08 +0100

    task 3

--- java_practice/src/main/java/com/javapractice/Main.java
+++ java_practice/src/main/java/com/javapractice/Main.java
@@ -1,4 +1,5 @@
 package com.javapractice;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,6 +9,10 @@ public class Main {
         Scanner scString= new Scanner(System.in);
         boolean continueCalculations = true;
         double totalCost=0;
+
+        //static array that grows
+        double[] prices = new double[5];
+        int entries=0;
 
         while (continueCalculations) {
             double cost;
@@ -20,12 +25,19 @@ public class Main {
                 System.out.print("Enter VAT rate (%): ");
                 vat = sc.nextDouble();
 
+                //if the index is now the size of the array we must increment it
+                if (entries== prices.length){
+                    //make prices the original prices with 2 more slots
+                    prices= Arrays.copyOf(prices,prices.length+2);
+                }
+
                 double finalVAT = VAT(vat, cost);
-                double finalPrice = finalVAT + cost;
+                prices[entries] = finalVAT + cost;
 
-                totalCost+=finalPrice;
+                totalCost+=prices[entries];
 
-                System.out.printf("This price is: £%.2f%n", finalPrice);
+
+                System.out.printf("This price is: £%.2f%n", prices[entries]);
                 System.out.printf("the total cost is: £%.2f%n",totalCost);
 
                 System.out.print("Type QUIT to stop or press ENTER to continue: ");
@@ -34,6 +46,10 @@ public class Main {
                 if (input.equalsIgnoreCase("QUIT")) {
                     continueCalculations = false;
                 }
+
+                //increment entries
+                entries++;
+
             } catch (InputMismatchException e) {
                 System.out.println("Incorrect value. Please enter valid numbers.");
                 sc.nextLine(); // Consume the invalid input
